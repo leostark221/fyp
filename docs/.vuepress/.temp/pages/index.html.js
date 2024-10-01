4bb3e5111ac42d80f797e20cfdbafdd4787f4704
leostark221 <ashair70@gmail.com>
2024-10-01 23:35:59 +0500

    doc web

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "D:/fypProject/vuepress-starter/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Home\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"Home\",\"heroImage\":\"/images/logo.png\",\"actions\":[{\"text\":\"Go to Docs\",\"link\":\"/get-started.html\",\"type\":\"primary\"}]},\"headers\":[],\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Home\",\"lang\":\"en-US\",\"frontmatter\":{\"home\":true,\"title\":\"Home\",\"heroImage\":\"/images/logo.png\",\"actions\":[{\"text\":\"Go to Docs\",\"link\":\"/get-started.html\",\"type\":\"primary\"}]},\"headers\":[],\"git\":{\"updatedTime\":1727805717000,\"contributors\":[{\"name\":\"leostark221\",\"email\":\"ashair70@gmail.com\",\"commits\":1}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
